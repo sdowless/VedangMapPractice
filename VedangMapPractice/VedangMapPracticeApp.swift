00cdfc165c5d866cd99e4c693a9e875d18b8f42c
Stephan Dowless <dowless.stephan@gmail.com>
2022-03-30 14:32:34 -0400

    Initial Commit

---
+++ VedangMapPractice/VedangMapPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VedangMapPracticeApp.swift
+//  VedangMapPractice
+//
+//  Created by Stephen Dowless on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct VedangMapPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
