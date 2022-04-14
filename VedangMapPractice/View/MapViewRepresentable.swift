016edf55c0026688d5e800a417748af06c855dad
Stephan Dowless <dowless.stephan@gmail.com>
2022-04-14 12:40:29 -0400

    Adding polyline

--- VedangMapPractice/View/MapViewRepresentable.swift
+++ VedangMapPractice/View/MapViewRepresentable.swift
@@ -25,6 +25,7 @@ struct MapViewRepresentable: UIViewRepresentable {
         mapView.isRotateEnabled = true
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        mapView.delegate = context.coordinator
         
         if let coordinate = locationManager.userLocation?.coordinate {
             
@@ -33,13 +34,16 @@ struct MapViewRepresentable: UIViewRepresentable {
                                                                    longitudeDelta: 0.05))
             
             mapView.setRegion(region, animated: true)
+            
         }
         
         return mapView
     }
     
+    // this function gets called everytime the view wants to update (publishers, notifications, etc.)
     func updateUIView(_ uiView: UIViewType, context: Context) {
-
+        guard let destinationPlacemark = viewModel.destinationPlacemark else { return }
+        context.coordinator.generatePolyline(forPlacemark: destinationPlacemark)
     }
     
     func makeCoordinator() -> MapCoordinator {
@@ -50,10 +54,73 @@ struct MapViewRepresentable: UIViewRepresentable {
 extension MapViewRepresentable {
     
     class MapCoordinator: NSObject, MKMapViewDelegate {
+        
+        // MARK: - Properties
+        
         let parent: MapViewRepresentable
+        
+        // MARK: - Lifecycle
         
         init(parent: MapViewRepresentable) {
             self.parent = parent
+        }
+        
+        // MARK: - MKMapViewDelegate Protocol Functions
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let overlay = MKPolylineRenderer(overlay: overlay)
+            overlay.strokeColor = .systemBlue
+            overlay.lineWidth = 6
+            return overlay
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            if let anno = annotation as? MKPointAnnotation {
+                let view = MKAnnotationView(annotation: anno, reuseIdentifier: "anno")
+                return view
+            }
+            
+            return nil
+        }
+        
+        // MARK: - Helpers
+        
+        func generatePolyline(forPlacemark destinationPlacemark: MKPlacemark) {
+            guard let userLocation = LocationManager.shared.userLocation else { return }
+            let userPlacemark = MKPlacemark(coordinate: userLocation.coordinate)
+            
+            print("DEBUG: User location is \(userLocation)")
+            
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destinationPlacemark)
+            
+            let directions = MKDirections(request: request)
+            
+            print("DEBUG: Directions \(directions)")
+            
+            directions.calculate { response, error in
+                print("DEBUG: Calculation success")
+                
+                if let error = error {
+                    print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
+                    return
+                }
+                
+                print("DEBUG: Directions response is \(response)")
+                
+                guard let polyline = response?.routes.first?.polyline else { return }
+                print("DEBUG: Polyline is \(polyline)")
+                self.parent.mapView.addOverlay(polyline)
+                
+                let anno = MKPointAnnotation()
+                anno.coordinate = destinationPlacemark.coordinate
+                self.parent.mapView.addAnnotation(anno)
+                
+                let rect = self.parent.mapView.mapRectThatFits(polyline.boundingMapRect,
+                                                               edgePadding: .init(top: 16, left: 16, bottom: 16, right: 16))
+                self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+            }
         }
     }
 }
--- VedangMapPractice/ViewModel/MapViewModel.swift
+++ VedangMapPractice/ViewModel/MapViewModel.swift
@@ -16,17 +16,16 @@ class MapViewModel: ObservableObject {
      and its associated data from view to view in our app.
      */
     var userLocation = LocationManager.shared.userLocation
+    @Published var destinationPlacemark: MKPlacemark?
     
     var addresses: [String] = [
-        "1976 Bascom Ave, Campbell, CA",
-        "1696 Tully Rd, San Jose, CA"
+        "1976 Bascom Ave, Campbell, CA"
+//        "1696 Tully Rd, San Jose, CA"
     ]
     
     var locationDistanceDict = [String: Double]()
     
-    init() {
-        print("DEBUG: User location is \(userLocation)")
-        
+    init() {        
         addresses.forEach { address in
             calculateRouteDistance(toAddress: address)
         }
@@ -40,6 +39,8 @@ class MapViewModel: ObservableObject {
         
         geocoder.geocodeAddressString(address) { placemarks, error in
             guard let endPlacemark = placemarks?.first else { return }
+            guard let coordinate = endPlacemark.location?.coordinate else { return }
+            self.destinationPlacemark = MKPlacemark(coordinate: coordinate)
             
             let start = MKMapItem(placemark: MKPlacemark(coordinate: userLocation.coordinate))
             let end = MKMapItem(placemark: MKPlacemark(coordinate: endPlacemark.location!.coordinate))
@@ -55,11 +56,11 @@ class MapViewModel: ObservableObject {
                 // Now we should have a route.
                 if let routes = response?.routes {
                     let route = routes[0]
-                    print("DEBUG: Distance between points is \(route.distance)")
+//                    print("DEBUG: Distance between points is \(route.distance)")
                     
                     self.locationDistanceDict[address] = route.distance
                     
-                    print("DEBUG: Location distances \(self.locationDistanceDict)")
+//                    print("DEBUG: Location distances \(self.locationDistanceDict)")
 
                 }
             })
